d152b234e1916b9f6860dd10470354fee3f7dee6
John Gabriel Singson <jgsingson@gmail.com>
2018-02-07 01:33:01 +0800

    finishing touches

--- addbook.php
+++ addbook.php
@@ -12,7 +12,32 @@ if (mysqli_connect_errno())
 	<title>Book Information</title>
     <link href="assets/css/reg.css" rel="stylesheet">
 </head>
+<style>
+		body{
+			background: #333;
+			font-family:arial;
+		}
+		form{
+			padding:10px;
+			margin:0 auto;
+			background:#FFFFFF;
+			width:500px;
+			align-items: center;
+			align-content: center;
+		}
+		div{
+			padding:10px;
+			margin:0 auto;
+			background:#FFFFFF;
+			width:1000px;
+		}
 
+		label{
+			font-weight:bold;
+			float:left;
+			width: 200px;
+		}
+	</style>
 <body>
 	<form action="" method="post">
 	<h1>Library Database</h1>
@@ -22,9 +47,29 @@ if (mysqli_connect_errno())
 		<label>Pages:</label> <input type="number" min=1 name="pages" required/><br />
 		<label>Author:</label> <input type="text" name="author" required/><br />
 		<label>Published Year:</label> <input type="text" name="year" required/>
-        <div><br/></div>
+        <br/>
+         <?php
+        if (
+            isset($_POST["title"]) &&
+            isset($_POST["pages"]) &&
+            isset($_POST["author"])  &&
+            isset($_POST["year"]) 
+
+
+           ){
+
+            $title = $_POST["title"];
+            $pages = $_POST["pages"];
+            $author  = $_POST["author"];
+            $year = $_POST["year"];
+            $query = "INSERT INTO books(Title,Pages,Author,PublishedYear) VALUES('$title','$pages','$author','year')";
+            mysqli_query($dbconn, $query);
+        }
+    ?>
     <input style="float:right" type="submit" value="Add Book" onClick="return submit_form();" name="submit"/>
     </fieldset>
+    </form>
+    <div>
     <h3>List of Stored Books</h3>
     <table border="2" align="center" cellpadding=5>
             <thead>
@@ -36,18 +81,40 @@ if (mysqli_connect_errno())
                 </tr>
             </thead>
             <tbody>
-                <?php
-                  
+            	
+                 <?php
+            $sql = "SELECT * FROM books";
+            if($result = mysqli_query($dbconn,$sql)){
+                while($rows = mysqli_fetch_assoc($result)){
+                	$ID = $rows["ID"];
+                    $Title = $rows["Title"];
+                    $Pages = $rows["Pages"];
+                    $Author = $rows["Author"];
+                    $PublishedYear = $rows["PublishedYear"];
+                    echo "
+                            <tr>
+                            <form action='account_handler.php' method='post'>
+                            <input type='hidden' name='ID' value='$ID' />
+                            <td><input type='text' class='flds-edit' value='$Title' name='Title'/></td>
+                            <td><input type='text' class='flds-edit' value='$Pages' name='Pages'/></td>
+                            <td><input type='text' class='flds-edit' value='$Author' name='Author'/></td>
+                            <td><input type='text' class='flds-edit' value='$PublishedYear' name='PublishedYear'/></td>
+                            <td><input type='submit' value='Update' name='update'/>
+                            <input type='submit' value='Delete' name='delete'/></td>
+                            </form>
+                            </tr>
+                    ";
+                }
+            }
+        ?>
+    </div>
             </tbody>
         </table>
-	</form>
     <script type="text/javascript" src="assets/js/jquery-1.10.2.js"></script>
 	<script type="text/javascript">
 		function submit_form(){
 			alert("A new book has been successfully added!");
 		}
 	</script>
-<?php 
-    $dbconn->close();
 </body>
 </html>
